f87c04cf63ac513fb9b0db0d0c786364bfaa355e
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-20 23:03:42 +0900

    Add coverage

--- ci/cover.js
+++ ci/cover.js
@@ -9,12 +9,10 @@
 process.chdir(`${__dirname}/..`)
 
 const { runTasks } = require('ape-tasking')
-const { measureCoverage } = require('ape-covering')
+const { coverage } = require('../lib')
 
 runTasks('cover', [
-  () => measureCoverage('_mocha', [
-    '-t', 6000, 'test/*_test.js'
-  ], {
+  () => coverage('test/*_test.js', {
     dir: 'coverage'
   })
 ], true)
