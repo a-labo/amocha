d4b8130242c0f1b93b1975107e5b080b7515ea23
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-21 09:01:04 +0900

    Version incremented to 1.0.8

--- example/example-usage.js
+++ example/example-usage.js
@@ -4,5 +4,11 @@ const amocha = require('amocha')
 const co = require('co')
 
 co(function * () {
+  // Run tests
   yield amocha('test/**/*_test.js', {})
+
+  // Measure coverage
+  yield amocha.coverage('test/**/*_test.js', {
+    dir: 'coverage'
+  })
 }).catch((err) => console.error(err))
